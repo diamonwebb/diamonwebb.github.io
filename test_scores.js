03de739badf98d1a51fc1da7421edb34e635fb5f
diamonwebb <143554249+diamonwebb@users.noreply.github.com>
2023-11-14 15:52:27 -0600

    j

--- test_scores.js
+++ test_scores.js
@@ -16,3 +16,21 @@ const html1 = `<p>Score 1 = ${score1}</p>
               <p>Score 3 = ${score3}</p>
               <p>Average score = ${average}</p>`;
 document.write(html1);
+let total = 0;
+
+const miles1 = parseInt(prompt("Enter miles per gallon"));
+total += miles1;        
+
+const miles2 = parseInt(prompt("Enter miles per gallon"));
+total += miles2;
+
+const score3 = parseInt(prompt("Enter miles per gallon"));
+total += miles3;
+
+const average = parseInt(total/3);
+
+const html1 = `<p>Miles 1 = ${miles1}</p>
+              <p>Miles 2 = ${miles2}</p>
+              <p>Miles 2 = ${miles3}</p>
+              <p>Average miles = ${average}</p>`;
+document.write(html1);
